a0bc4b94ea5b1c11fcec402e38a0f63e35e8cdf3
Akis Foulidis <ben.whatsisname@gmail.com>
2015-01-09 21:37:04 +0100

    comment out Stage rules

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
-stage { 'first': before => Stage['main'] }
-stage { 'last': require => Stage['main'] }
+#stage { 'first': before => Stage['main'] }
+#stage { 'last': require => Stage['main'] }
 
 node default {
   include roles::webserver
